d624220bc7224c5ca962a1d77ef2020f661dd836
Florent Guilleux <florent2@gmail.com>
2013-11-24 01:56:37 -0500

    Use Mandrill to deliver emails in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,14 @@ Larpwriter::Application.configure do
   config.action_mailer.default_url_options = { host: ENV['HOST'] }
 
   config.assets.precompile += %w( *.svg *.eot *.woff *.ttf *.png )
+
+  ActionMailer::Base.smtp_settings = {
+    port:           '587',
+    address:        'smtp.mandrillapp.com',
+    user_name:      ENV['MANDRILL_USERNAME'],
+    password:       ENV['MANDRILL_APIKEY'],
+    domain:         'heroku.com',
+    authentication: :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
